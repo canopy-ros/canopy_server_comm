d7f13dcee0adcdf4f59967565ae8780ba48b9b24
Raphael Chang <raphaelchang1227@gmail.com>
2016-02-27 03:28:38 -0500

    Removed test code and replaced tabs with spaces

--- receiver.go
+++ receiver.go
@@ -1,135 +1,128 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
-	"log"
-	"bytes"
-	"io"
-	"compress/zlib"
-	"encoding/json"
-	"time"
-	"strings"
-	"regexp"
-	"fmt"
-	rethink "github.com/dancannon/gorethink"
+    "github.com/gorilla/websocket"
+    "log"
+    "bytes"
+    "io"
+    "compress/zlib"
+    "encoding/json"
+    "time"
+    "strings"
+    "regexp"
+    "fmt"
 )
 
 type receiver struct {
-	ws *websocket.Conn
-	process chan []byte
-	h *hub
-	s *rethink.Session
-	name string
-	private_key string
-	to []string
-	msg_type string
-	rcvFreq float32
-	sendFreq map[*sender]float32
+    ws *websocket.Conn
+    process chan []byte
+    h *hub
+    name string
+    private_key string
+    to []string
+    msg_type string
+    rcvFreq float32
+    sendFreq map[*sender]float32
 }
 
 type message struct {
-	To []string
-	From string
-	Topic string
-	Type string
-	Stamp float64
-	Msg json.RawMessage
+    To []string
+    From string
+    Topic string
+    Type string
+    Stamp float64
+    Msg json.RawMessage
 }
 
 type description struct {
-	Data string
+    Data string
 }
 
 // processor from receiver decompresses the packet and unmarshals the JSON
 // to retrieve destination information. It then forwards the original packet
 // to the desired client senders.
 func (r *receiver) processor() {
-	for {
-		msg := <- r.process
-		snd := sendChannel{r: r, data: msg}
-		rdr, err := zlib.NewReader(bytes.NewBuffer(msg))
-		if err != nil {
-			break;
-		}
-		var out bytes.Buffer
-		io.Copy(&out, rdr)
-		rdr.Close()
-		decompressed := out.Bytes()
-		var m message
-		json.Unmarshal(decompressed[4:], &m)
-		//log.Println("To:", m.To)
-		r.msg_type = m.Type
-		split := strings.Split(m.Topic, "/")
-		if (split[len(split) - 1] == "description") {
-			var d description
-			json.Unmarshal(m.Msg, &d)
-			if sender, ok := r.h.senderMap[r.private_key][split[1]]; ok {
-				sender.description = d.Data
-			}
-		}
-		for _, to := range m.To {
-			list := make([]string, 0)
-			if sender, ok := r.h.senderMap[r.private_key][to]; ok {
-				list = append(list, to)
-				select {
-				case sender.send <- snd:
-				default:
-				}
-			} else { // Regex
-				for name, sender := range r.h.senderMap[r.private_key] {
-					if name != m.From {
-						match, _ := regexp.MatchString(to, name)
-						if match {
-							list = append(list, name)
-							select {
-							case sender.send <- snd:
-							default:
-							}
-						}
-					}
-				}
-			}
-			r.to = list
-		}
-		_, err = rethink.DB("test").Table("test").Get(2).Update(
-			map[string]interface{}{
-            "title":   "Lorem ipsum",
-		    "content": "Dolor sit amet",
-		}).RunWrite(r.s)
-		if err != nil {
-		    fmt.Print(err)
-		    return
-		}
-	}
+    for {
+        msg := <- r.process
+        snd := sendChannel{r: r, data: msg}
+        rdr, err := zlib.NewReader(bytes.NewBuffer(msg))
+        if err != nil {
+            break;
+        }
+        var out bytes.Buffer
+        io.Copy(&out, rdr)
+        rdr.Close()
+        decompressed := out.Bytes()
+        var m message
+        json.Unmarshal(decompressed[4:], &m)
+        //log.Println("To:", m.To)
+        r.msg_type = m.Type
+        split := strings.Split(m.Topic, "/")
+        if (split[len(split) - 1] == "description") {
+            var d description
+            json.Unmarshal(m.Msg, &d)
+            if sender, ok := r.h.senderMap[r.private_key][split[1]]; ok {
+                sender.description = d.Data
+            }
+        }
+        for _, to := range m.To {
+            list := make([]string, 0)
+            if sender, ok := r.h.senderMap[r.private_key][to]; ok {
+                list = append(list, to)
+                select {
+                case sender.send <- snd:
+                default:
+                }
+            } else { // Regex
+                for name, sender := range r.h.senderMap[r.private_key] {
+                    if name != m.From {
+                        match, _ := regexp.MatchString(to, name)
+                        if match {
+                            list = append(list, name)
+                            select {
+                            case sender.send <- snd:
+                            default:
+                            }
+                        }
+                    }
+                }
+            }
+            r.to = list
+        }
+        if err != nil {
+            fmt.Print(err)
+            return
+        }
+    }
 }
 
 // reader from receiver continually polls the socket for new packets,
 // and then sends them to be processed. It also calculates read frequencies.
 func (r *receiver) reader() {
-	count := 0
-	last_time := time.Now()
-	for {
-		_, message, err := r.ws.ReadMessage()
-		if err != nil {
-    			log.Printf("[%s] ReadError: %s", r.name, err)
-			break
-		}
-		select {
-		case r.process <- message:
-		default:
-		}
-		//log.Printf("%s: %d", r.name, unsafe.Sizeof(message))
-		msg := make([]byte, 1)
-		err = r.ws.WriteMessage(websocket.BinaryMessage, msg)
-		if err != nil {
-			break
-		}
-		count += 1
-		if count == 20 {
-			r.rcvFreq = 20.0 * 1e9 / float32((time.Now().Sub(last_time)))
-			last_time = time.Now()
-			count = 0
-		}
-	}
-	r.ws.Close()
+    count := 0
+    last_time := time.Now()
+    for {
+        _, message, err := r.ws.ReadMessage()
+        if err != nil {
+                log.Printf("[%s] ReadError: %s", r.name, err)
+            break
+        }
+        select {
+        case r.process <- message:
+        default:
+        }
+        //log.Printf("%s: %d", r.name, unsafe.Sizeof(message))
+        msg := make([]byte, 1)
+        err = r.ws.WriteMessage(websocket.BinaryMessage, msg)
+        if err != nil {
+            break
+        }
+        count += 1
+        if count == 20 {
+            r.rcvFreq = 20.0 * 1e9 / float32((time.Now().Sub(last_time)))
+            last_time = time.Now()
+            count = 0
+        }
+    }
+    r.ws.Close()
 }
--- sender.go
+++ sender.go
@@ -1,74 +1,74 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
-	"log"
-	"time"
+    "github.com/gorilla/websocket"
+    "log"
+    "time"
 )
 
 type sendChannel struct {
-	r *receiver
-	data []byte
+    r *receiver
+    data []byte
 }
 
 type sender struct {
-	ws *websocket.Conn
-	send chan sendChannel
-	name string
-	private_key string
-	description string
+    ws *websocket.Conn
+    send chan sendChannel
+    name string
+    private_key string
+    description string
 }
 
 // heartbeat from sender sends a heartbeat packet to the client every second,
 // when no regular packets are being sent.
 func (s *sender) heartbeat(beat chan bool, stop chan bool) {
-	for {
-		exit := false
-		select {
-		case <-beat:
-		case <-time.After(500 * time.Millisecond):
-			message := make([]byte, 1)
-			s.send <- sendChannel{r: &receiver{}, data: message}
-		case <-stop:
-			exit = true
-		}
-		if exit {
-			break
-		}
-	}
+    for {
+        exit := false
+        select {
+        case <-beat:
+        case <-time.After(500 * time.Millisecond):
+            message := make([]byte, 1)
+            s.send <- sendChannel{r: &receiver{}, data: message}
+        case <-stop:
+            exit = true
+        }
+        if exit {
+            break
+        }
+    }
 }
 
 // writer from sender writes packets to the receiving client.
 // It also calculates write frequencies.
 func (s *sender) writer() {
-	hbeat := make(chan bool)
-	stop := make(chan bool)
-	go s.heartbeat(hbeat, stop)
-	count := make(map[*receiver]int32)
-	last_time := make(map[*receiver]time.Time)
-	for message := range s.send {
-		select {
-		case hbeat <- true:
-		default:
-		}
-		err := s.ws.WriteMessage(websocket.BinaryMessage, message.data)
-		if err != nil {
-			log.Printf("[%s] WriteError: %s", s.name, err)
-			break
-		}
-		if _, ok := count[message.r]; !ok {
-			count[message.r] = 0
-			last_time[message.r] = time.Now()
-		}
-		count[message.r] += 1
-		if count[message.r] >= 20 {
-			message.r.sendFreq[s] = 20.0 * 1e9 / float32(
-				(time.Now().Sub(last_time[message.r])))
-			last_time[message.r] = time.Now()
-			count[message.r] = 0
+    hbeat := make(chan bool)
+    stop := make(chan bool)
+    go s.heartbeat(hbeat, stop)
+    count := make(map[*receiver]int32)
+    last_time := make(map[*receiver]time.Time)
+    for message := range s.send {
+        select {
+        case hbeat <- true:
+        default:
+        }
+        err := s.ws.WriteMessage(websocket.BinaryMessage, message.data)
+        if err != nil {
+            log.Printf("[%s] WriteError: %s", s.name, err)
+            break
+        }
+        if _, ok := count[message.r]; !ok {
+            count[message.r] = 0
+            last_time[message.r] = time.Now()
+        }
+        count[message.r] += 1
+        if count[message.r] >= 20 {
+            message.r.sendFreq[s] = 20.0 * 1e9 / float32(
+                (time.Now().Sub(last_time[message.r])))
+            last_time[message.r] = time.Now()
+            count[message.r] = 0
                 }
 
-	}
-	stop <- true
-	s.ws.Close()
+    }
+    stop <- true
+    s.ws.Close()
 }
