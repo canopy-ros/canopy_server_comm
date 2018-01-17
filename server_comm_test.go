b0f05e6ceee6c497b5a991d934176d60fe83745a
Zareen Choudhury <zareenc@mit.edu>
2018-01-17 16:44:46 -0500

    Add test for handler

--- server_comm_test.go
+++ server_comm_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
@@ -29,4 +31,49 @@ func TestServerComm(t *testing.T) {
 	ticker.Stop()
 	srv.Close()
 	log.Println("Server has been closed")
+}
+
+// TestServerCommHandler tests the communication
+// server handler's response to a valid request.
+func TestServerCommHandler(t *testing.T) {
+
+	// initialize server
+	db = "none"
+	log.Println(db)
+	r := &wsHandler{h: newHub()}
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+	/*
+			srv := &http.Server{
+				Addr:    ":8000",
+				Handler: r,
+			}
+		    resp, err := http.Get(srv.URL)
+	*/
+
+	// create mock request
+	url := fmt.Sprintf("%s/PsFXjmWpszr6acSKL/test_robot/state", srv.URL)
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Connection", "Upgrade")
+	req.Header.Set("Upgrade", "websocket")
+	req.Header.Set("Sec-Websocket-Version", "13")
+	req.Header.Set("Sec-Websocket-Key", "7sCORs4/dvKiJRJb+vcZCA==")
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	// send request and inspect response
+	/*
+		resp := httptest.NewRecorder()
+		h.ServeHTTP(resp, req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		log.Println("Status code:", resp.Code)
+		log.Println("Response:", resp)
+	*/
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Println("Status code:", resp.StatusCode)
+	log.Println("Response:", resp)
 }
