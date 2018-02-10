7a7ada3755b5bf84f3f66226f51316b943aef33e
raphaelchang <raphaelchang1227@gmail.com>
2018-02-10 16:27:20 -0500

    Code review changes

--- sender.go
+++ sender.go
@@ -1,106 +1,106 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
-	"log"
-	"time"
+    "github.com/gorilla/websocket"
+    log "github.com/sirupsen/logrus"
+    "time"
 )
 
-type sendChannel struct {
-	r    *receiver
-	data []byte
+type sendPacket struct {
+    r    *receiver
+    data []byte
 }
 
 type sender struct {
-	ws          *websocket.Conn
-	h           *hub
-	send        chan sendChannel
-	name        string
-	shortname   string
-	privateKey  string
-	description string
-	freqs       map[*receiver]float32
+    ws          *websocket.Conn
+    h           *hub
+    send        chan sendPacket
+    name        string
+    shortname   string
+    privateKey  string
+    description string
+    freqs       map[*receiver]float32
 }
 
 type senderMessage struct {
-	PrivateKey  string
-	Description string
-	Name        string
-	Freqs       map[string]float32
+    PrivateKey  string
+    Description string
+    Name        string
+    Freqs       map[string]float32
 }
 
 // heartbeat from sender sends a heartbeat packet to the client every second,
 // when no regular packets are being sent.
 func (s *sender) heartbeat(beat chan bool, stop chan bool) {
-	for {
-		exit := false
-		select {
-		case <-beat:
-		case <-time.After(500 * time.Millisecond):
+    for {
+        exit := false
+        select {
+        case <-beat:
+        case <-time.After(500 * time.Millisecond):
             message := make([]byte, 2)
-			s.send <- sendChannel{r: &receiver{}, data: message}
-		case <-stop:
-			exit = true
-		}
-		if exit {
-			break
-		}
-	}
+            s.send <- sendPacket{r: &receiver{}, data: message}
+        case <-stop:
+            exit = true
+        }
+        if exit {
+            break
+        }
+    }
 }
 
 // writer from sender writes packets to the receiving client.
 // It also calculates write frequencies.
 func (s *sender) writer() {
-	hbeat := make(chan bool)
-	stop := make(chan bool)
-	go s.heartbeat(hbeat, stop)
-	count := make(map[*receiver]int32)
-	lastTime := make(map[*receiver]time.Time)
-	if db != dbNone {
-		s.h.dbw.AddKey(false, "clients:"+s.name+":privateKey", s.privateKey)
-		s.h.dbw.AddKey(false, "clients:"+s.name+":description", s.description)
-		s.h.dbw.AddKey(false, "clients:"+s.name+":name", s.shortname)
-	}
+    hbeat := make(chan bool)
+    stop := make(chan bool)
+    go s.heartbeat(hbeat, stop)
+    count := make(map[*receiver]int32)
+    lastTime := make(map[*receiver]time.Time)
+    if db != dbNone {
+        s.h.dbw.AddKey(false, "clients:"+s.name+":privateKey", s.privateKey)
+        s.h.dbw.AddKey(false, "clients:"+s.name+":description", s.description)
+        s.h.dbw.AddKey(false, "clients:"+s.name+":name", s.shortname)
+    }
 
-	for message := range s.send {
-		select {
-		case hbeat <- true:
-		default:
-		}
+    for message := range s.send {
+        select {
+        case hbeat <- true:
+        default:
+        }
 
-		err := s.ws.WriteMessage(websocket.BinaryMessage, message.data)
-		if err != nil {
-			log.Printf("[%s] WriteError: %s", s.name, err)
-			break
-		}
-		if _, ok := count[message.r]; !ok {
-			count[message.r] = 0
-			lastTime[message.r] = time.Now()
-		}
-		count[message.r]++
-		if count[message.r] >= 20 {
-			s.freqs[message.r] = 20.0 * 1e9 / float32(
-				(time.Now().Sub(lastTime[message.r])))
-			lastTime[message.r] = time.Now()
-			count[message.r] = 0
-		}
+        err := s.ws.WriteMessage(websocket.BinaryMessage, message.data)
+        if err != nil {
+            log.Printf("[%s] WriteError: %s", s.name, err)
+            break
+        }
+        if _, ok := count[message.r]; !ok {
+            count[message.r] = 0
+            lastTime[message.r] = time.Now()
+        }
+        count[message.r]++
+        if count[message.r] >= 20 {
+            s.freqs[message.r] = 20.0 * 1e9 / float32(
+                (time.Now().Sub(lastTime[message.r])))
+            lastTime[message.r] = time.Now()
+            count[message.r] = 0
+        }
         // s.ws.SetReadDeadline(time.Now().Add(2000 * time.Millisecond))
         _, _, err = s.ws.ReadMessage()
         if err != nil {
-            log.Printf("[%s] AcknowledgeTimeout: %s", s.name, err)
-			break
+            log.WithFields(log.Fields{"name": s.name, "error": err}).Info("Acknowledge timeout")
+            break
         }
 
-		if db != dbNone {
-			s.h.dbw.AddKey(false, "clients:"+s.name+":privateKey", s.privateKey)
-			s.h.dbw.AddKey(false, "clients:"+s.name+":description", s.description)
-			s.h.dbw.AddKey(false, "clients:"+s.name+":name", s.shortname)
-			for key, value := range s.freqs {
-				s.h.dbw.AddKey(false, "clients:"+s.name+":freq:"+
-					key.name[len("/"+key.privateKey):], value)
-			}
-		}
-	}
-	stop <- true
-	s.ws.Close()
+        if db != dbNone {
+            s.h.dbw.AddKey(false, "clients:"+s.name+":privateKey", s.privateKey)
+            s.h.dbw.AddKey(false, "clients:"+s.name+":description", s.description)
+            s.h.dbw.AddKey(false, "clients:"+s.name+":name", s.shortname)
+            for key, value := range s.freqs {
+                s.h.dbw.AddKey(false, "clients:"+s.name+":freq:"+
+                    key.name[len("/"+key.privateKey):], value)
+            }
+        }
+    }
+    stop <- true
+    s.ws.Close()
 }
