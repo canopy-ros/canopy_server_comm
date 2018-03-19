29dd66a84b42f88231124cc9d16c6e53985b6d6e
raphaelchang <raphaelchang1227@gmail.com>
2018-03-19 02:12:54 -0400

    More refactoring

--- client.go
+++ client.go
@@ -1,148 +1,192 @@
 package main
 
 import (
-	"bytes"
-	"compress/zlib"
-	"encoding/json"
-	"io"
-	"net"
-	"regexp"
-	"strings"
+    "bytes"
+    "compress/zlib"
+    "encoding/json"
+    "io"
+    "net"
+    "regexp"
+    "strings"
 
-	"github.com/canopy-ros/canopy_server_comm/loggers"
-	log "github.com/sirupsen/logrus"
+    "github.com/canopy-ros/canopy_server_comm/loggers"
+    log "github.com/sirupsen/logrus"
 )
 
 // Client defines a client connection object
 type Client struct {
-	process    chan []byte
-	h          *hub
-	addr       *net.UDPAddr
-	name       string
-	privateKey string
-	msgType    string
-	rcvFreq    float32
-	description string
-	rateLoggers map[string]*loggers.RateLogger
+    process    chan []byte
+    hub        *Hub
+    addr       *net.UDPAddr
+    name       string
+    privateKey string
+    msgType    string
+    rcvFreq    float32
+    description string
+    rateLoggers map[string]*loggers.RateLogger
 }
 
-type message struct {
-	To         []string
-	From       string
-	Topic      string
-	Type       string
-	Stamp      float64
-	Msg        json.RawMessage
-	PrivateKey string
+// Message represents a ROS message from a client
+type Message struct {
+    To         []string
+    From       string
+    Topic      string
+    Type       string
+    Stamp      float64
+    Msg        json.RawMessage
+    PrivateKey string
 }
 
-type description struct {
-	Data string
+// Description defines a struct for a description message from a client
+type Description struct {
+    Data string
 }
 
 // DescriptionTopic is the topic name of the client description
 const DescriptionTopic = "description"
 // LeafletPrefix is the prefix of leaflet client names
 const LeafletPrefix = "canopy_leaflet_"
+// DecompressedHeaderIndex is the start index of the decompressed packet
+const DecompressedHeaderIndex = 4
 
-// processor from client decompresses the packet and unmarshals the JSON
+// Processor from client decompresses the packet and unmarshals the JSON
 // to retrieve destination information. It then forwards the original packet
-// to the desired client senders.
-func (c *Client) processor() {
-	lastTime := 0.0
-	for {
-        msg := <-c.process
-		rdr, err := zlib.NewReader(bytes.NewBuffer(msg))
-		if err != nil {
-            log.WithFields(log.Fields{
-                "error": err,
-            }).Fatal("Decompress error")
-			continue
-		}
-		var out bytes.Buffer
-		io.Copy(&out, rdr)
-		rdr.Close()
-		decompressedRaw := out.Bytes()
-        decompressed := decompressedRaw[4:]
-		var m message
-		json.Unmarshal(decompressed, &m)
+// to the desired clients.
+func (c *Client) Processor() {
+    lastTime := 0.0
+    for {
+        packet := <-c.process
+        msg, err := c.UnpackMessage(&packet)
+        if err != nil {
+            continue
+        }
 
-		if _, ok := c.rateLoggers[m.Topic]; !ok {
-			c.rateLoggers[m.Topic] = loggers.NewRateLogger(100)
-		}
+        if _, ok := c.rateLoggers[msg.Topic]; !ok {
+            c.rateLoggers[msg.Topic] = loggers.NewRateLogger(RateLoggerRate)
+        }
 
-		c.rateLoggers[m.Topic].Log("Client frequency", log.Fields{
-			"topic": m.Topic,
-			"from":  m.From,
-		})
+        c.rateLoggers[msg.Topic].Log("Client frequency", log.Fields{
+            "topic": msg.Topic,
+            "from":  msg.From,
+        })
 
-		// Ensure message is coming from correct client.
-		if m.From != c.name || m.PrivateKey != c.privateKey {
-			continue
-		}
-		// Ensure messages are sent in order.
-		if m.Stamp < lastTime {
-			continue
-		}
-		lastTime = m.Stamp
-		c.msgType = m.Type
-		splitTopic := strings.Split(m.Topic, "/")
-        topicName := splitTopic[len(splitTopic)-1]
-        clientName := splitTopic[1]
-		if topicName == DescriptionTopic {
-			var d description
-			json.Unmarshal(m.Msg, &d)
-			if sender, ok := c.h.clientMap[c.privateKey][clientName]; ok {
-				sender.description = d.Data
-			}
-		}
-		for name := range c.h.clientMap[c.privateKey] {
-			if strings.HasPrefix(name, LeafletPrefix) {
-				m.To = append(m.To, name)
-			}
-		}
-		list := make([]string, 0)
-		for _, to := range m.To {
-			if sender, ok := c.h.clientMap[c.privateKey][to]; ok {
-				exists := false
-				for _, check := range list {
-					if check == to {
-						exists = true
-						break
-					}
-				}
-				if !exists {
-					list = append(list, to)
-					snd := SendPacket{addr: sender.addr, data: append([]byte{}, msg...)}
-					select {
-					case c.h.sendChannel <- snd:
-					default:
-					}
-				}
-			} else { // Regex
-				for name, sender := range c.h.clientMap[c.privateKey] {
-					if name != m.From {
-						match, _ := regexp.MatchString(to, name)
-						if match {
-							exists := false
-							for _, check := range list {
-								if check == name {
-									exists = true
-									break
-								}
-							}
-							if !exists {
-								list = append(list, name)
-								snd := SendPacket{addr: sender.addr, data: append([]byte{}, msg...)}
-								select {
-								case c.h.sendChannel <- snd:
-								default:
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+        // Ensure message is coming from correct client.
+        if msg.From != c.name || msg.PrivateKey != c.privateKey {
+            continue
+        }
+        // Ensure messages are sent in order.
+        if msg.Stamp < lastTime {
+            continue
+        }
+        lastTime = msg.Stamp
+        c.msgType = msg.Type
+        topicName, clientName := processTopicString(msg.Topic)
+        if topicName == DescriptionTopic {
+            err = c.ProcessDescription(&msg, clientName)
+            if err != nil {
+                continue
+            }
+        }
+        for name := range c.hub.clientMap[c.privateKey] {
+            if strings.HasPrefix(name, LeafletPrefix) {
+                msg.To = append(msg.To, name)
+            }
+        }
+        c.SendMessage(&msg, &packet)
+    }
+}
+
+// UnpackMessage converts raw packet data into a Message struct
+func (c *Client) UnpackMessage(packet *[]byte) (Message, error) {
+    rdr, err := zlib.NewReader(bytes.NewBuffer(*packet))
+    if err != nil {
+        log.WithFields(log.Fields{
+            "error": err,
+        }).Fatal("Decompress error")
+        return Message{}, err
+    }
+    var out bytes.Buffer
+    io.Copy(&out, rdr)
+    rdr.Close()
+    decompressedRaw := out.Bytes()
+    decompressed := decompressedRaw[DecompressedHeaderIndex:]
+    var m Message
+    err = json.Unmarshal(decompressed, &m)
+    if err != nil {
+        log.WithFields(log.Fields{
+            "error": err,
+        }).Fatal("Unmarshal error")
+        return Message{}, err
+    }
+    return m, nil
+}
+
+// SendMessage sends a packet to another client based on destination data
+func (c *Client) SendMessage(m *Message, packet *[]byte) {
+    sent := make([]string, 0)
+    for _, to := range m.To {
+        if sender, ok := c.hub.clientMap[c.privateKey][to]; ok {
+            exists := false
+            for _, check := range sent {
+                if check == to {
+                    exists = true
+                    break
+                }
+            }
+            if !exists {
+                sent = append(sent, to)
+                snd := SendPacket{addr: sender.addr, data: append([]byte{}, *packet...)}
+                select {
+                case c.hub.sendChannel <- snd:
+                default:
+                }
+            }
+        } else { // Regex
+            for name, sender := range c.hub.clientMap[c.privateKey] {
+                if name != m.From {
+                    match, _ := regexp.MatchString(to, name)
+                    if match {
+                        exists := false
+                        for _, check := range sent {
+                            if check == name {
+                                exists = true
+                                break
+                            }
+                        }
+                        if !exists {
+                            sent = append(sent, name)
+                            snd := SendPacket{addr: sender.addr, data: append([]byte{}, *packet...)}
+                            select {
+                            case c.hub.sendChannel <- snd:
+                            default:
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// ProcessDescription processes a description message and updates client descriptions
+func (c *Client) ProcessDescription(m *Message, clientName string) error {
+    var desc Description
+    err := json.Unmarshal(m.Msg, &desc)
+    if err != nil {
+        log.WithFields(log.Fields{
+            "error": err,
+        }).Fatal("Unmarshal error")
+        return err
+    }
+    if sender, ok := c.hub.clientMap[c.privateKey][clientName]; ok {
+        sender.description = desc.Data
+    }
+    return nil
+}
+
+func processTopicString(topic string) (string, string) {
+    splitTopic := strings.Split(topic, "/")
+    topicName := splitTopic[len(splitTopic)-1]
+    clientName := splitTopic[1]
+    return topicName, clientName
 }
--- main.go
+++ main.go
@@ -2,15 +2,14 @@
 package main
 
 import (
-	"flag"
-	"net"
-	"strings"
-	"syscall"
+    "flag"
+    "net"
+    "strings"
 
-	"github.com/canopy-ros/canopy_server_comm/loggers"
-	"github.com/garyburd/redigo/redis"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
+    "github.com/canopy-ros/canopy_server_comm/loggers"
+    "github.com/garyburd/redigo/redis"
+    log "github.com/sirupsen/logrus"
+    "github.com/spf13/viper"
 )
 
 // SendPacket defines a packet to be sent
@@ -19,150 +18,173 @@ type SendPacket struct {
     data []byte
 }
 
-// hub is a connection point between a network of senders and receivers.
+// Hub is a connection point between a network of senders and receivers.
 // Communication between senders and receivers is logged to a database
 // if a database writer is specified.
-type hub struct {
+type Hub struct {
     clientMap   map[string]map[string]*Client
     sendChannel chan SendPacket
-	dbw         DBWriter
+    dbw         DBWriter
 }
 
-// newHub creates a new hub object.
-func newHub() *hub {
-	return &hub{
-		clientMap: make(map[string]map[string]*Client),
-	}
+// NewHub creates a new Hub object.
+func NewHub() *Hub {
+    return &Hub{
+        clientMap: make(map[string]map[string]*Client),
+    }
 }
 
-func udpServer(address string, h *hub) {
-	serverAddr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		panic(err)
-	}
-	serverConn, err := net.ListenUDP("udp", serverAddr)
-	if err != nil {
-		panic(err)
-	}
-	serverConn.SetReadBuffer(65535)
-	serverConn.SetWriteBuffer(65535)
-	file, _ := serverConn.File()
-	defer file.Close()
-	syscall.SetsockoptInt(int(file.Fd()), syscall.SOL_IP, 10, 0)
-	defer serverConn.Close()
+// ConnectionBufferSize sets the buffer size for packets
+const ConnectionBufferSize = 65535
 
-	h.sendChannel = make(chan SendPacket, 5)
-	addrMap := make(map[string]*Client)
-	go sender(serverConn, h.sendChannel)
-	buf := make([]byte, 65507)
+// RateLoggerRate defines the rate to log rates at
+const RateLoggerRate = 100
 
-	rateLoggers := make(map[string]*loggers.RateLogger)
+// UdpServer sets up a UDP server and listens for connections
+func UdpServer(address string, hub *Hub) {
+    serverConn := SetupConnection(address)
+    defer serverConn.Close()
 
-	for {
-		n, addr, err := serverConn.ReadFromUDP(buf)
-		if err != nil {
+    hub.sendChannel = make(chan SendPacket, 5)
+    addrMap := make(map[string]*Client)
+    go Sender(serverConn, hub.sendChannel)
+    buf := make([]byte, ConnectionBufferSize)
+
+    rateLoggers := make(map[string]*loggers.RateLogger)
+
+    for {
+        n, addr, err := serverConn.ReadFromUDP(buf)
+        if err != nil {
             log.WithFields(log.Fields{
                 "error": err,
             }).Fatal("UDP read error")
-		}
-		if strings.HasPrefix(string(buf[:n]), "CONNECT") {
-			split := strings.Split(string(buf[:n]), ":")
-			privateKey := split[1]
-			name := split[2]
-            log.WithFields(log.Fields{
-                "privateKey": privateKey,
-                "name": name,
-            }).Info("New client")
-			h.sendChannel <- SendPacket{addr: addr, data: []byte("HANDSHAKE")}
-			if _, ok := h.clientMap[privateKey]; !ok {
-				h.clientMap[privateKey] = make(map[string]*Client)
-			}
-			if _, ok := h.clientMap[privateKey][name]; !ok {
-				cli := &Client{addr: addr, process: make(chan []byte, 1),
-					name: name, privateKey: privateKey, h: h,
-					rateLoggers: make(map[string]*loggers.RateLogger)}
-				h.clientMap[privateKey][name] = cli
-				addrMap[string(addr.IP)+":"+string(addr.Port)] = cli
-				go cli.processor()
-			} else {
-				h.clientMap[privateKey][name].addr = addr
-				addrMap[string(addr.IP)+":"+string(addr.Port)] = h.clientMap[privateKey][name]
-			}
-		} else {
-			addrStr := string(addr.IP) + ":" + string(addr.Port)
-			if _, ok := addrMap[addrStr]; ok {
-				if _, ok := rateLoggers[addrStr]; !ok {
-					rateLoggers[addrStr] = loggers.NewRateLogger(100)
-				}
-
-				rateLoggers[addrStr].Log("Before process", log.Fields{
-					"addr": addr.String(),
-				})
-
-				select {
-				case addrMap[addrStr].process <- append([]byte{}, buf[:n]...):
-				default:
-				}
-			}
-		}
-	}
+        }
+        packet := buf[:n]
+        if strings.HasPrefix(string(packet), "CONNECT") {
+            NewClient(addr, &packet, &addrMap, hub)
+        } else {
+            ProcessDataPacket(addr, &packet, &addrMap, &rateLoggers)
+        }
+    }
 }
 
-func sender(c *net.UDPConn, s <-chan SendPacket) {
-	rateLogger := loggers.NewRateLogger(100)
-	for message := range s {
-		_, err := c.WriteToUDP(message.data, message.addr)
+// SetupConnection sets up the UDP server
+func SetupConnection(address string) *net.UDPConn {
+    serverAddr, err := net.ResolveUDPAddr("udp", address)
+    if err != nil {
+        panic(err)
+    }
+    serverConn, err := net.ListenUDP("udp", serverAddr)
+    if err != nil {
+        panic(err)
+    }
+    serverConn.SetReadBuffer(ConnectionBufferSize)
+    serverConn.SetWriteBuffer(ConnectionBufferSize)
+    return serverConn
+}
 
-		rateLogger.Log("Sender frequency", log.Fields{})
+// NewClient handles connect packets and creates client objects
+func NewClient(addr *net.UDPAddr, connectPacket *[]byte, addrMap *map[string]*Client, hub *Hub) {
+    split := strings.Split(string(*connectPacket), ":")
+    privateKey := split[1]
+    name := split[2]
+    log.WithFields(log.Fields{
+        "privateKey": privateKey,
+        "name": name,
+    }).Info("New client")
+    hub.sendChannel <- SendPacket{addr: addr, data: []byte("HANDSHAKE")}
+    if _, ok := hub.clientMap[privateKey]; !ok {
+        hub.clientMap[privateKey] = make(map[string]*Client)
+    }
+    addrStr := string(addr.IP) + ":" + string(addr.Port)
+    if _, ok := hub.clientMap[privateKey][name]; !ok {
+        cli := &Client{addr: addr, process: make(chan []byte, 1),
+        name: name, privateKey: privateKey, hub: hub,
+        rateLoggers: make(map[string]*loggers.RateLogger)}
+        hub.clientMap[privateKey][name] = cli
+        (*addrMap)[addrStr] = cli
+        go cli.Processor()
+    } else {
+        hub.clientMap[privateKey][name].addr = addr
+        (*addrMap)[addrStr] = hub.clientMap[privateKey][name]
+    }
+}
 
-		if err != nil {
+// ProcessDataPacket receives data packets and forwards them to clients
+func ProcessDataPacket(addr *net.UDPAddr, packet *[]byte, addrMap *map[string]*Client, rateLoggers *map[string]*loggers.RateLogger) {
+    addrStr := string(addr.IP) + ":" + string(addr.Port)
+    if _, ok := (*addrMap)[addrStr]; ok {
+        if _, ok := (*rateLoggers)[addrStr]; !ok {
+            (*rateLoggers)[addrStr] = loggers.NewRateLogger(RateLoggerRate)
+        }
+
+        (*rateLoggers)[addrStr].Log("Before process", log.Fields{
+            "addr": addr.String(),
+        })
+
+        select {
+        case (*addrMap)[addrStr].process <- append([]byte{}, *packet...):
+        default:
+        }
+    }
+}
+
+// Sender is the thread for sending to clients
+func Sender(c *net.UDPConn, s <-chan SendPacket) {
+    rateLogger := loggers.NewRateLogger(100)
+    for message := range s {
+        _, err := c.WriteToUDP(message.data, message.addr)
+
+        rateLogger.Log("Sender frequency", log.Fields{})
+
+        if err != nil {
             log.WithFields(log.Fields{
                 "error": err,
             }).Fatal("UDP write error")
-		}
-	}
+        }
+    }
 }
 
 // Options for the 'db' key in the config file
 const (
-	dbRedis string = "redis"
-	dbNone  string = "none"
+    dbRedis string = "redis"
+    dbNone  string = "none"
 )
 
 var db string
 var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
-	log.Println("Canopy communication server started.")
+    log.Println("Canopy communication server started.")
 
-	// get config
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("/etc/canopy/")
-	viper.SetConfigName("config")
-	viper.SetDefault("db", dbNone)
-	viper.ReadInConfig()
-	db = viper.GetString("db")
+    // get config
+    viper.AddConfigPath(".")
+    viper.AddConfigPath("/etc/canopy/")
+    viper.SetConfigName("config")
+    viper.SetDefault("db", dbNone)
+    viper.ReadInConfig()
+    db = viper.GetString("db")
 
-	h := newHub()
+    h := NewHub()
 
-	// initialize database writer
-	switch db {
-	case dbRedis:
-		log.Println("Initializing redis.")
-		c, err := redis.Dial("tcp", ":6379")
-		if err != nil {
-			panic(err)
-		}
-		dbw := &redisWriter{conn: &c, commChannel: make(chan command, 2)}
-		h.dbw = dbw
-		c.Do("DEL", "clients:list")
-		defer func() {
-			c.Do("DEL", "clients:list")
-			c.Close()
-		}()
-		go dbw.Writer()
-	default:
-		log.Println("No database specified.")
-	}
-	udpServer(*addr, h)
+    // initialize database writer
+    switch db {
+    case dbRedis:
+        log.Println("Initializing redis.")
+        c, err := redis.Dial("tcp", ":6379")
+        if err != nil {
+            panic(err)
+        }
+        dbw := &redisWriter{conn: &c, commChannel: make(chan command, 2)}
+        h.dbw = dbw
+        c.Do("DEL", "clients:list")
+        defer func() {
+            c.Do("DEL", "clients:list")
+            c.Close()
+        }()
+        go dbw.Writer()
+    default:
+        log.Println("No database specified.")
+    }
+    UdpServer(*addr, h)
 }
